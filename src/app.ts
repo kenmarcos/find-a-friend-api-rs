554d109a186e2ddf3747b9ad120e705990e91ee2
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2024-08-04 19:08:10 -0300

    refactor: adjust message error

--- src/app.ts
+++ src/app.ts
@@ -8,12 +8,12 @@ app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
     return reply
       .status(400)
-      .send({ message: "Validation Error", issues: error.format() });
+      .send({ message: "Validation Error.", issues: error.format() });
   }
 
   if (env.NODE_ENV === "production") {
     console.error(error);
   }
 
-  return reply.status(500).send({ message: "Internal Server Error" });
+  return reply.status(500).send({ message: "Internal Server Error." });
 });
