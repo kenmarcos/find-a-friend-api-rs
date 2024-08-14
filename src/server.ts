a26123b5e14e04f5913183dc79207eb13f48b83a
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2024-08-14 16:14:42 -0300

    feat: add database connection check

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,27 @@
 import { app } from "./app";
 import { env } from "./env";
+import { prisma } from "./lib/prisma";
 
-app
-  .listen({
-    host: "0.0.0.0",
-    port: env.PORT,
-  })
-  .then(() => {
-    console.log(`Server is running on http://localhost:${env.PORT}`);
-  });
+async function startServer() {
+  try {
+    // Tenta conectar ao banco de dados
+    await prisma.$connect();
+    console.log("Database connected.");
+
+    // Inicia o servidor somente após a conexão ser bem-sucedida
+    app
+      .listen({
+        host: "0.0.0.0",
+        port: env.PORT,
+      })
+      .then(() => {
+        console.log(`Server is running on http://localhost:${env.PORT}`);
+      });
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+    console.log("❌ Server is not running.");
+  }
+}
+
+// Chama a função para iniciar o servidor
+startServer();
